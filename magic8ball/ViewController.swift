0963c4b1fc68f04b42b53d7d6259c1692dd5da08
Amit Tandel <46502349+itsmrtandel@users.noreply.github.com>
2023-03-21 15:25:23 -0700

    initial commit

---
+++ magic8ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  magic8ball
+//
+//  Created by Amit Tandel on 3/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let ballArray = [#imageLiteral(resourceName: "ball1.png"), #imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball7"), #imageLiteral(resourceName: "ball6")]
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        imageView.image = ballArray[Int.random(in: 0...6)]
+    }
+}
+
+
+
